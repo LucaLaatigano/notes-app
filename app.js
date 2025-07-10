d1ee263c9b246f7f30d3abe0eb65185bf5acd37d
Luca Latigano <lucalatigano12@gmail.com>
2025-07-10 20:06:14 -0300

    added the delete button and change sonme styles

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const createBtn = document.getElementById("create-btn")
 const noteDiv = document.getElementById("notes")
 const createDiv = document.getElementById("message")
+const noteName = document.getElementById("note-name")
 const notes = []
 function createNote(container, name, date) {
     const note = document.createElement("div")
+    const nameAndDate = document.createElement("div")
+    nameAndDate.id = "name-date"
     note.classList.add("note")
     const createdName = document.createElement("h3")
     createdName.classList.add("note-name")
@@ -11,14 +14,48 @@ function createNote(container, name, date) {
     const dateContent = document.createElement("p")
     dateContent.classList.add("date")
     dateContent.textContent = date
+    const buttons = document.createElement("div")
+    buttons.id = "div-buttons-note"
+    const deleteBtn = document.createElement("button")
+    deleteBtn.textContent = "🗑️"
+    deleteBtn.classList.add("deleted")
+    const editBtn = document.createElement("button")
+    editBtn.textContent ="✏️"
 
-    note.appendChild(createdName)
-    note.appendChild(dateContent)
+    nameAndDate.appendChild(createdName)
+    nameAndDate.appendChild(dateContent)
 
-    container.appendChild(note)    
+    buttons.appendChild(deleteBtn)
+    buttons.appendChild(editBtn)
+
+    note.appendChild(nameAndDate)
+    note.appendChild(buttons)
+
+    container.appendChild(note) 
+}
+
+function deleteAnote(dBtn){
+    dBtn.forEach((deleteButton)=> {
+
+        const newButtonWithoutPastEvents = deleteButton.cloneNode(true)
+        deleteButton.replaceWith(newButtonWithoutPastEvents)
+
+        newButtonWithoutPastEvents.addEventListener("click", (e)=>{
+            if(e.target.classList.contains("deleted")){
+                const noteAboutToBeRemoved = e.target.closest(".note")
+                if(noteAboutToBeRemoved && noteDiv && noteDiv.contains(noteAboutToBeRemoved)){
+                    noteDiv.removeChild(noteAboutToBeRemoved)
+                } else{
+                    console.log("no papa eligio la incorrecta")
+                }
+            }
+        })
+    });
+    
 }
 
 createBtn.addEventListener("click", () =>{
+    noteName.value = ""
     createDiv.style.display = "flex"
 })
 
@@ -27,7 +64,6 @@ create.addEventListener("click", () => {
     if(createDiv.style.display === "flex"){
         createDiv.style.display = "none"
     }
-    const noteName = document.getElementById("note-name")
     const name = noteName.value
     const date = new Date()
     const dateTrimed = date.toString().split(" ")
@@ -35,8 +71,9 @@ create.addEventListener("click", () => {
     
     notes.push(name)
     createNote(noteDiv, name, newDate)
+    const deleteBtnFromNotes = document.querySelectorAll(".deleted")
+    deleteAnote(deleteBtnFromNotes)
 })
-
 
 
 const backBtn = document.getElementById("backBtn")
@@ -44,3 +81,5 @@ backBtn.addEventListener("click", () =>{
     createDiv.style.display = "none"
 })
 
+
+console.log(notes)
